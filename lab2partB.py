296fd25259544b9ee0d3c3f716995846b2648239
Noemi Elizabeth Hernandez <42879006+NEHernandez4@users.noreply.github.com>
2018-10-23 13:59:47 -0600

    Update lab2partB.py

--- lab2partB.py
+++ lab2partB.py
@@ -4,8 +4,8 @@
 # Lab 2 Option B
 # Submitted By: Noemi Hernandez
 # Date last edited: 23 October 2018
-# The purpose of this program is to read from a file a list
-# user names and passwords, and check the list to see how many
+# The purpose of this program is to read from a file a list of
+# user names and passwords, then check the list to see how many
 # times a certain password was use, then print out the top
 # 20 most used passwords from that list
 
